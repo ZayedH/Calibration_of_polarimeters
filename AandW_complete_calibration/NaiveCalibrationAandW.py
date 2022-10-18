e9793ba99a27965e70f32ba6e469c32ec1c8e7bc
Zayed Herma <zayed@MacBook-Pro-de-Zayed.local>
2022-10-18 21:48:38 +0200

    data

--- AandW_complete_calibration/NaiveCalibrationAandW.py
+++ AandW_complete_calibration/NaiveCalibrationAandW.py
@@ -1,30 +1,38 @@
+import time
 import sys
 sys.path.insert(1, 'AandW_pixel_calibration')
-from Calibration_W import Calibration_W
 from Calibration_A import Calibration_A
-import numpy as np
+from Calibration_W import Calibration_W
+
+sys.path.insert(1, 'Preprocesssing')
+from DataMatToNp import *
 
 
 
-
-def calibrationAandW(M_0, M_1, M_2, M_3, B_0, B_1, B_2, B_3):
-    n = np.shape(B_0)[0]
-    A = np.zeros((n, n, 4, 4))
-    W = np.zeros((n, n, 4, 4))
+def calibrationAandW():
     for i in range(n):
         for j in range(n):
-            B0_pixel = B_0[i, j, :]
-            B1_pixel = B_1[i, j, :]
-            B2_pixel = B_2[i, j, :]
-            B3_pixel = B_3[i, j, :]
+            B0_pixel = b0[i, j, :]
+            B1_pixel = b1[i, j, :]
+            B2_pixel = b2[i, j, :]
+            B3_pixel = b3[i, j, :]
             # Make pixel calibration
             A_pixel = Calibration_A(
-                M_0, M_1, M_2, M_3, B0_pixel, B1_pixel, B2_pixel, B3_pixel)
+                M_Air, M_Pol0, M_Pol90, M_Ret30, B0_pixel, B1_pixel, B2_pixel, B3_pixel)
             W_pixel = Calibration_W(
-                M_0, M_1, M_2, M_3, B0_pixel, B1_pixel, B2_pixel, B3_pixel)
-            A[i, j, :] = A_pixel
-            W[i, j, :] = W_pixel
-            print('i ', i)
-            print('j ', j)
+                M_Air, M_Pol0, M_Pol90, M_Ret30, B0_pixel, B1_pixel, B2_pixel, B3_pixel)
+            A[i, j] = A_pixel
+            W[i, j] = W_pixel
+            # print('i ', i)
+            # print('j ', j)
 
-    return A, W
+    return 
+
+
+t1 = time.perf_counter()
+calibrationAandW()
+t2 = time.perf_counter()
+print("naive: ",t2 - t1)
+
+
+
