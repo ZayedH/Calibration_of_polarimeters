58276b122fa4f73d056de9b117dfed48f0480775
Zayed Herma <zayed@MacBook-Pro-de-Zayed.local>
2022-11-22 22:25:06 +0100

    cool

--- AandW_pixel_calibration/Calibration_A.py
+++ AandW_pixel_calibration/Calibration_A.py
@@ -7,7 +7,7 @@ def Calibration_A(M_0, M_1, M_2, M_3, B_0, B_1, B_2, B_3):
     eigenvalues, eigenvectors = np.linalg.eigh(KA)
     v = eigenvectors[:, 0]
     # v0=np.max(v)
-    # v = (1/v[0])*v
+    # v = (1/v0)*v
 
     return v.reshape((4, 4))
 
